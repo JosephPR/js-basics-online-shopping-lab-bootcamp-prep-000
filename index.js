d834b47cd843d3b4069cfd6ee6a77e8dd5446a52
Joseph Reilly <reilly4181@gmail.com>
2019-04-01 21:37:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,12 +9,6 @@ function setCart() {
 }
 
 function addToCart(item) {
-    var price = Math.floor(Math.random() * 100) + 1;
-  var item_object = {};
-  item_object[item] = price
-  cart.push(item_object)
-  console.log(item + " has been added to your cart.");
-  return cart
 
 }
 
