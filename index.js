1e94ce5e93323695b8972817ba2469b86fe3faee
Joseph Reilly <reilly4181@gmail.com>
2019-04-01 21:38:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,29 +1,108 @@
 var cart = [];
 
-function getCart() {
-
+ function getCart() {
+  return cart;
 }
 
-function setCart() {
 
+ function setCart(newCart) {	function setCart(newCart) {
+  cart = newCart;	  cart = newCart;
+}	}
+
+
+ function addToCart(item) {
+  var price = Math.floor(Math.random() * 100) + 1;
+  var item_object = {};
+  item_object[item] = price
+  cart.push(item_object)
+  console.log(item + " has been added to your cart.");
+  return cart
 }
 
-function addToCart(item) {
-
+ function viewCart() {
+  if (cart.length === 0) {
+    console.log("Your shopping cart is empty.")
+  } else {
+    var items = [];
+    for (var i = 0; i < cart.length; i++) {
+      for (var item in cart[i]) {
+        items.push(item + " at $" + cart[i][item])
+      }
+    }
+    console.log("In your cart, you have " + items.join(", ") + ".");
+  }
 }
 
-function viewCart() {
-  // write your code here
+ function removeFromCart(item) {
+  var itemInCart = false
+  for(var i = 0; i < cart.length; i++){
+    if (cart[i].hasOwnProperty(item)) {
+      itemInCart = true;
+      cart.splice(i, 1);
+    }
+  }
+  if (!itemInCart) {
+    console.log("That item is not in your cart.");
+  }
+  return cart
 }
 
-function total() {
-  // write your code here
+ function total() {	function total() {
+  let t = 0
+
+
+ @@ -15,3 +58,12 @@ function total() {
+
+
+   return t	  
+}	}
+
+ function placeOrder(creditCard) {
+  if (!creditCard) {
+    console.log("We don't have a credit card on file for you to place your order.");
+  } else {
+    console.log("Your total cost is $" + total() + ", which will be charged to the card " + creditCard + ".");
+    cart = [];
+  }
 }
 
-function removeFromCart(item) {
-  // write your code here
-}
 
-function placeOrder(cardNumber) {
-  // write your code here
-}
+
+
+
+
+
+
+
+
+
+
+// var cart = [];
+
+// function getCart() {
+
+// }
+
+// function setCart() {
+
+// }
+
+// function addToCart(item) {
+
+// }
+
+// function viewCart() {
+//   // write your code here
+// }
+
+// function total() {
+//   // write your code here
+// }
+
+// function removeFromCart(item) {
+//   // write your code here
+// }
+
+// function placeOrder(cardNumber) {
+//   // write your code here
+// }
