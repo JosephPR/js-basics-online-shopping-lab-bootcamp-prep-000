a4c44ce2b9e987ff8fe9e44a14d6a09c21fa6e94
Joseph Reilly <reilly4181@gmail.com>
2019-04-01 21:39:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,30 +1,30 @@
 
-// var cart = [];
+var cart = [];
 
-// function getCart() {
+function getCart() {
 
-// }
+}
 
-// function setCart() {
+function setCart() {
 
-// }
+}
 
-// function addToCart(item) {
+function addToCart(item) {
 
-// }
+}
 
-// function viewCart() {
-//   // write your code here
-// }
+function viewCart() {
+  // write your code here
+}
 
-// function total() {
-//   // write your code here
-// }
+function total() {
+  // write your code here
+}
 
-// function removeFromCart(item) {
-//   // write your code here
-// }
+function removeFromCart(item) {
+  // write your code here
+}
 
-// function placeOrder(cardNumber) {
-//   // write your code here
-// }
+function placeOrder(cardNumber) {
+  // write your code here
+}
